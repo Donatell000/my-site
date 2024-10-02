99a1cb9ed1779b812b80d256e0188663466c0d36
Misha Martushev <pochta@gmail.com>
2024-10-02 20:09:23 +0500

    добавлены модели пользователя и поста, установлен фреймворк Simple JWT и его начальные настройки

--- blog/blog/models.py
+++ blog/blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(models.Model):
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.username
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
